3df1a7f2e0e775e3412f8f5e8604b28d04604d14
DESKTOP-DKSP6I4\HaMinhDat <ha_minh_dat@allgrow-labo.jp>
2022-11-08 17:59:39 +0700

    design SP

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,9 +1,11 @@
 
 $(document).ready(function(){
   // Set div display to none
-  $(".c-point__btn").click(function(){
-      $(".c-img__inner").css("display", "block");
-  });
+  // $(".c-point__btn").click(function(){
+  //     $(".c-img__inner").css("display", "block");
+  // });
+  
+
   
 
   
