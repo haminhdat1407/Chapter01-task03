7bd1765bc1d4f8d77033f09c09cf32d6f0d6bf86
minhdatagl <ha_minh_dat@allgrow-labo.jp>
2022-11-15 00:46:13 +0700

    done

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,18 +1,23 @@
 
 $(document).ready(function () {
 
+  //Close menu
+  $('.c-navlinksp').click(function () {
+    var parent = $(this).parent().parent();
+    var grand = $(parent).find('.l-overlay');
+    $('.c-input').first().trigger('click');
+  })
+
   // Active Nav
-  $('.c-navlist li a').click(function(e) {
-    $('.c-navlist li.is-active').removeClass('is-active');
+  // $('.c-navlist li a').click(function (e) {
+  //   $('.c-navlist li.is-active').removeClass('is-active');
 
-    var $parent = $(this).parent();
-    $parent.addClass('is-active');
-});
+  //   var $parent = $(this).parent();
+  //   $parent.addClass('is-active');
+  // });
 
-
+  // Button POINT
   $('.c-point__btn').click(function () {
-
-       // Button POINT
     var parent = $(this).parent().parent();
     var grand = parent.parent();
     grand.find('.c-img__inner').fadeOut();
@@ -20,10 +25,10 @@ $(document).ready(function () {
 
   });
 
-       // Show Hide Des
+  // Show Hide Des
   $('.c-btnarrow').click(function () {
     var parent = $(this).parent();
-    var childArrow=$(parent).find('.c-iconarrow')[0];
+    var childArrow = $(parent).find('.c-iconarrow')[0];
     var child = $(parent).find('.c-deslink')[0];
     $(child).toggle();
     if ($(childArrow).hasClass('js-iconarrow')) {
@@ -32,11 +37,11 @@ $(document).ready(function () {
       $(childArrow).addClass('js-iconarrow');
     }
   })
-  
+
   //Scroll leaf and drop
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 5) {
-        $('.c-drop').addClass('is-active');
+      $('.c-drop').addClass('is-active');
       $('.c-leaftop').addClass('is-active');
       $('.c-leafright').addClass('is-active');
     } else {
@@ -47,4 +52,43 @@ $(document).ready(function () {
   });
 
 
+  //Active Menu When Scroll
+  $(document).on("scroll", onScroll);
+
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function () {
+      $(this).removeClass('is-active');
+    })
+    $(this).addClass('is-active');
+
+    var target = this.hash, 
+    menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - 10
+    },'swing', function () {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
+    });
+  });
 });
+
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop();
+  $('#menu-link a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('#menu-link li a ').removeClass("is-active");
+      currLink.addClass("is-active");
+    }
+    else {
+      currLink.removeClass("is-active");
+    }
+  });
+}
+
